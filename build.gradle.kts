9aa3bcc0b07e2bc5f49147bf6a6aa99860b54655
danielmi5 <midaniel@iesrafaelalberti.es>
2025-03-22 20:00:29 +0100

    Gradle importado

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "2.0.21"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(22) // Asegura que se usa la misma versión para Kotlin y Java
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = "22"
+    targetCompatibility = "22"
+}
