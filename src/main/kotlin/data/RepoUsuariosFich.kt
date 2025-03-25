028f4db71c90aa0aaa248a15012bb6cf76a6977d
danielmi5 <midaniel@iesrafaelalberti.es>
2025-03-25 17:28:58 +0100

    data ficheros en desarrollo

--- src/main/kotlin/data/RepoUsuariosFich.kt
+++ src/main/kotlin/data/RepoUsuariosFich.kt
@@ -3,10 +3,14 @@ package data
 import model.Usuario
 import utils.IUtilFicheros
 
-class RepoUsuariosFich(private val fich: IUtilFicheros) : RepoUsuariosMem(), ICargarUsuariosIniciales {
+class RepoUsuariosFich(private val rutaArchivo: String, private val fich: IUtilFicheros) : RepoUsuariosMem(), ICargarUsuariosIniciales {
+    val usuarios: MutableList<Usuario> = mutableListOf()
 
     override fun cargarUsuarios(): Boolean {
-
+        val usuariosCargados = fich.leerArchivo(rutaArchivo)
+        usuariosCargados.forEach {
+            
+        }
     }
 
     override fun eliminar(usuario: Usuario): Boolean {
--- src/main/kotlin/utils/Ficheros.kt
+++ src/main/kotlin/utils/Ficheros.kt
@@ -1,30 +1,42 @@
 package utils
-
+import java.io.File
 import model.IExportable
 import model.Seguro
 
+
 class Ficheros : IUtilFicheros {
     override fun leerArchivo(ruta: String): List<String> {
-        TODO("Not yet implemented")
+        val file = File(ruta)
+        return file.bufferedReader().readLines()
     }
 
     override fun leerSeguros(ruta: String, mapaSeguros: Map<String, (List<String>) -> Seguro>): List<Seguro> {
-        TODO("Not yet implemented")
+        val lista = leerArchivo(ruta)
+        lista.forEach {
+            val datos = it.split(";")
+        }
     }
 
     override fun agregarLinea(ruta: String, linea: String): Boolean {
-        TODO("Not yet implemented")
+        val file = File(ruta)
+        file.bufferedWriter().newLine()
+        file.writeText(linea)
+        return true
     }
 
     override fun <T : IExportable> escribirArchivo(ruta: String, elementos: List<T>): Boolean {
-        TODO("Not yet implemented")
+        elementos.forEach {
+            if (!agregarLinea(ruta, it.serializar())) return false
+        }
+        return true
     }
 
     override fun existeFichero(ruta: String): Boolean {
-        TODO("Not yet implemented")
+        return File(ruta).isFile
     }
 
     override fun existeDirectorio(ruta: String): Boolean {
-        TODO("Not yet implemented")
+        val file = File(ruta)
+        return file.isDirectory
     }
 }
